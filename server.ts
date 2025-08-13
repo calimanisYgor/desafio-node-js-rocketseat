ad4ad2c27c175d8fd0c761cba834e9156e14d274
calimanisYgor <ygoralvescalimanis@hotmail.com>
2025-08-13 10:33:32 -0300

    Add PostgreSQL support with Drizzle ORM and set up initial database schema
    - Created .env.example for environment variables.
    - Added docker-compose.yml for PostgreSQL service configuration.
    - Implemented drizzle.config.ts for database configuration.
    - Updated package.json and package-lock.json to include necessary dependencies (drizzle-orm, pg, drizzle-kit).
    - Refactored server.ts to use Drizzle ORM for database interactions.
    - Created initial database schema for users and courses in SQL files.
    - Added database client setup in src/database/client.ts and schema definitions in src/database/schema.ts.

--- server.ts
+++ server.ts
@@ -1,28 +1,32 @@
 import fastify from "fastify";
-import crypto from "node:crypto";
+import { eq } from "drizzle-orm";
+import { db } from "./src/database/client.ts";
+import { courses } from "./src/database/schema.ts";
+
 const server = fastify({
-    logger: {
-      transport: {
-        target: 'pino-pretty',
-        options: {
-          translateTime: 'HH:MM:ss Z',
-          ignore: 'pid,hostname',
-        },
+  logger: {
+    transport: {
+      target: "pino-pretty",
+      options: {
+        translateTime: "HH:MM:ss Z",
+        ignore: "pid,hostname",
       },
     },
-  })
-
-const courses = [
-  { id: "1", title: "Curso Node.Js" },
-  { id: "2", title: "Curso React" },
-  { id: "3", title: "Curso React Native" },
-];
-
-server.get("/courses", () => {
-  return { courses };
+  },
 });
 
-server.get("/courses/:id", (request, reply) => {
+server.get("/courses", async (request, reply) => {
+  const result = await db
+    .select({
+      id: courses.id,
+      title: courses.title,
+    })
+    .from(courses);
+
+  return reply.send({ courses: result });
+});
+
+server.get("/courses/:id", async (request, reply) => {
   type Params = {
     id: string;
   };
@@ -31,25 +35,42 @@ server.get("/courses/:id", (request, reply) => {
 
   const courseId = params.id;
 
-  const course = courses.find((course) => course.id === courseId);
+  const result = await db
+    .select()
+    .from(courses)
+    .where(eq(courses.id, courseId));
 
-  if (course) {
-    return { course };
+  if (result.length > 0) {
+    return { course: result[0] };
   }
 
   return reply.status(401).send();
 });
 
-server.post("/courses", (request, reply) => {
+server.post("/courses", async (request, reply) => {
   type Body = {
-
+    title: string;
   };
-  const courseId = crypto.randomUUID();
-  courses.push({ id: courseId, title: "curso" });
 
-  return reply.status(201).send(courseId);
+  const body = request.body as Body;
+  const courseTitle = body.title;
+
+  if (!courseTitle) {
+    return reply
+      .status(400)
+      .send({ message: "Título do curso é obrigatório!" });
+  }
+
+  const result = await db
+    .insert(courses)
+    .values({
+      title: courseTitle,
+    })
+    .returning();
+
+  return reply.status(201).send(result[0].id);
 });
 
 server.listen({ port: 3333 }).then(() => {
-  console.log("HTTP srever running!");
+  console.log("HTTP server running!");
 });
