a82aebff6b35e791f0fe39ff4014fdd4896acc07
calimanisYgor <ygoralvescalimanis@hotmail.com>
2025-08-16 15:50:11 -0300

    Add seeding functionality and enhance get-courses route
    - Introduced a new seeding script to populate the database with sample users, courses, and enrollments using @faker-js/faker.
    - Updated the get-courses route to include enrollment counts for each course, enhancing the response structure.
    - Modified the database schema to support the new enrollments table and its relationships.

---
+++ src/database/seed.ts
@@ -0,0 +1,46 @@
+import { db } from "./client.ts";
+import { courses, enrollments, users } from "./schema.ts";
+import { fakerPT_BR as faker
+ } from "@faker-js/faker";
+
+async function seed() {
+  const usersInsert = await db
+    .insert(users)
+    .values([
+      { name: faker.person.fullName(), email: faker.internet.email() },
+      { name: faker.person.fullName(), email: faker.internet.email() },
+      { name: faker.person.fullName(), email: faker.internet.email() },
+    ])
+    .returning();
+
+  const programmingLanguages = [
+    "JavaScript",
+    "Python",
+    "Java",
+    "C++",
+    "TypeScript",
+    "Go",
+    "Rust",
+    "Ruby",
+    "PHP",
+    "C#",
+  ];
+
+  const coursesInsert = await db
+    .insert(courses)
+    .values([
+      { title: `Curso de ${faker.helpers.arrayElement(programmingLanguages)}` },
+      { title: `Curso de ${faker.helpers.arrayElement(programmingLanguages)}` },
+    ])
+    .returning();
+
+  await db.insert(enrollments).values([
+    { courseId: coursesInsert[0].id, userId: usersInsert[0].id },
+    { courseId: coursesInsert[0].id, userId: usersInsert[1].id },
+    { courseId: coursesInsert[1].id, userId: usersInsert[2].id },
+  ]);
+
+  await db.$client.end()
+}
+
+seed();
